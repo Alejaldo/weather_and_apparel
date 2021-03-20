d30ccd9f2b740efb3c9e2dcd8e2057f4e261682b
Alejaldo <statfluct@yahoo.com>
2021-03-20 13:13:33 +0500

    Remove while loop from main.rb

--- main.rb
+++ main.rb
@@ -3,25 +3,19 @@ require_relative 'lib/apparel_collection'
 
 apparels = ApparelCollection.from_dir("#{__dir__}/data/*.txt")
 
-keep_going = true
+puts "Сколько градусов за окном? (можно с минусом)"
 
-while keep_going do
+user_input = STDIN.gets.to_i
 
-  puts "Сколько градусов за окном? (можно с минусом)"
+if user_input.between?(apparels.whole_temp_range.min, apparels.whole_temp_range.max)
+  puts "\nДля #{user_input} °С предлагаем сегодня надеть:\n\n"
 
-  user_input = STDIN.gets.to_i
+  apparels.types.map { |type| apparels.group_by_type(type) }.
+  map { |apparel_group| apparel_group.select { |apparel| apparel.suitable?(user_input) } }.
+  each { |apparels| puts apparels.sample if apparels.size > 0 }
 
-  if user_input.between?(apparels.whole_temp_range.min, apparels.whole_temp_range.max)
-    puts "\nПредлагаем сегодня надеть:\n\n"
-
-    apparels.types.map { |type| apparels.group_by_type(type) }.
-    map { |apparel_group| apparel_group.select { |apparel| apparel.suitable?(user_input) } }.
-    each { |apparels| puts apparels.sample if apparels.size > 0 }
-
-    keep_going = false
-  else
-    puts "\nНаши технические возможности позволяют пока что давать рекоммендации только для температурного диапазона:"
-    puts "#{apparels.whole_temp_range.min} .. +#{apparels.whole_temp_range.max} °С"
-    puts "\nПробуем еще раз ...\n\n"
-  end
+  keep_going = false
+else
+  puts "\nИзвините, но наши технические возможности позволяют пока что давать рекоммендации только для температурного диапазона:"
+  puts "#{apparels.whole_temp_range.min} .. +#{apparels.whole_temp_range.max} °С"
 end
