c4d1a586cf224f264b204a5369d2548a7dd8d5cc
Alejaldo <statfluct@yahoo.com>
2021-03-21 13:03:28 +0500

    Do refactoring

--- main.rb
+++ main.rb
@@ -10,11 +10,7 @@ user_input = STDIN.gets.to_i
 if user_input.between?(apparels.whole_temp_range.min, apparels.whole_temp_range.max)
   puts "\nДля #{user_input} °С предлагаем сегодня надеть:\n\n"
 
-  apparels.types.map { |type| apparels.group_by_type(type) }.
-  map { |apparel_group| apparel_group.select { |apparel| apparel.suitable?(user_input) } }.
-  each { |apparels| puts apparels.sample if apparels.size > 0 }
-
-  keep_going = false
+  apparels.choose_suitable(user_input).each { |apparels| puts apparels.sample if apparels.size > 0 }
 else
   puts "\nИзвините, но наши технические возможности позволяют пока что давать рекоммендации только для температурного диапазона:"
   puts "#{apparels.whole_temp_range.min} .. +#{apparels.whole_temp_range.max} °С"
