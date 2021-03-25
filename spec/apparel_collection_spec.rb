1bfe621c1aab0dda9335d8b62860153e6a929e1b
Alejaldo <statfluct@yahoo.com>
2021-03-25 12:51:11 +0500

    Init some code for ApparelCollection class

--- spec/apparel_collection_spec.rb
+++ spec/apparel_collection_spec.rb
@@ -2,5 +2,13 @@ require 'apparel_collection'
 require 'apparel'
 
 describe ApparelCollection do
- 
+  before do
+    @apparels = ApparelCollection.from_dir("#{__dir__}/data/*.txt")
+  end
+
+  describe '#whole_temp_range' do
+  end
+
+  describe '#choose_suitable' do
+  end
 end
