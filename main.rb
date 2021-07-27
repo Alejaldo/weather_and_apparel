1be9e8ab9ab8ec286bff6d599340fc674e29994f
Alejaldo <statfluct@yahoo.com>
2021-07-27 11:28:57 +0500

    Add new class and gems for for automatic temperature detection

--- main.rb
+++ main.rb
@@ -1,16 +1,19 @@
 require_relative 'lib/apparel'
 require_relative 'lib/apparel_collection'
+require_relative 'lib/temperature_getter'
+require 'dotenv/load'
+
+OPENWEATHERMAP_API_KEY = ENV['OPENWEATHERMAP_API_KEY'].freeze
 
 apparels = ApparelCollection.from_dir("#{__dir__}/data/*.txt")
+user_temp = TemperatureGetter.call(api_key: OPENWEATHERMAP_API_KEY).round
 
-puts "Сколько градусов за окном? (можно с минусом)"
+puts "Сейчас у Вас #{user_temp}°С"
 
-user_input = STDIN.gets.to_i
+if user_temp.between?(apparels.whole_temp_range.min, apparels.whole_temp_range.max)
+  puts "\nДля #{user_temp}°С предлагаем сегодня надеть:\n\n"
 
-if user_input.between?(apparels.whole_temp_range.min, apparels.whole_temp_range.max)
-  puts "\nДля #{user_input} °С предлагаем сегодня надеть:\n\n"
-
-  apparels.choose_suitable(user_input).each { |type, apparels| puts apparels.sample }
+  apparels.choose_suitable(user_temp).each { |type, apparels| puts apparels.sample }
 else
   puts "\nИзвините, но наши технические возможности позволяют пока что давать рекоммендации только для температурного диапазона:"
   puts "#{apparels.whole_temp_range[0]} .. +#{apparels.whole_temp_range[1]} °С"
