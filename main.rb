9d890489fcb13fe27d4a0935925f5037cea56757
Alejaldo <statfluct@yahoo.com>
2021-07-30 10:12:28 +0500

    Adjust output text

--- main.rb
+++ main.rb
@@ -8,7 +8,7 @@ OPENWEATHERMAP_API_KEY = ENV['OPENWEATHERMAP_API_KEY'].freeze
 apparels = ApparelCollection.from_dir("#{__dir__}/data/*.txt")
 user_temp = TemperatureGetter.call(api_key: OPENWEATHERMAP_API_KEY).round
 
-puts "Сейчас у Вас (Your temperature now is) #{user_temp}°С"
+puts "Сейчас у Вас (The temperature outside is) #{user_temp}°С"
 
 if user_temp.between?(apparels.whole_temp_range.min, apparels.whole_temp_range.max)
   puts "\nДля #{user_temp}°С предлагаем сегодня надеть (For #{user_temp}°С we suggest you should wear):\n\n"
